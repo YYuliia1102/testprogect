8fdea292686ef3b072cb562f2b225041459fafbe
diua80 <119079479+diua80@users.noreply.github.com>
2023-04-29 08:46:00 +0000

    Deploying to gh-pages from @ YYuliia1102/testprogect@38009d722f6955fd770f0e6d073ec53190eb14ae 🚀

---
+++ index.25ecc176.js
@@ -0,0 +1,2 @@
+const e={form:document.querySelector(".todo"),list:document.querySelector(".todo__items")},o={};e.form.addEventListener("input",(function({target:e}){const{name:t,value:n}=e;o[t]=n,console.log(o)}));
+//# sourceMappingURL=index.25ecc176.js.map
---
+++ index.b8dad5e9.js
@@ -0,0 +1,2 @@
+!function(){var e={form:document.querySelector(".todo"),list:document.querySelector(".todo__items")},o={};e.form.addEventListener("input",(function(e){var t=e.target,n=t.name,r=t.value;o[n]=r,console.log(o)}))}();
+//# sourceMappingURL=index.b8dad5e9.js.map
