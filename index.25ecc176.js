336df3eacef8f8bd0f880b2c2e0cf6a0237f3b13
Yaroslav-Hryshenko <117099783+Yaroslav-Hryshenko@users.noreply.github.com>
2023-04-29 09:16:18 +0000

    Deploying to gh-pages from @ YYuliia1102/testprogect@c4f149c13d8c51d70fefaac341914901f7b15713 🚀

--- index.25ecc176.js
+++
@@ -1,2 +0,0 @@
-const e={form:document.querySelector(".todo"),list:document.querySelector(".todo__items")},o={};e.form.addEventListener("input",(function({target:e}){const{name:t,value:n}=e;o[t]=n,console.log(o)}));
-//# sourceMappingURL=index.25ecc176.js.map
